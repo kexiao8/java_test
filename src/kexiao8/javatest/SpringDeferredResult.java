7443e322747f00d63c5062a7a5707f1865ef4f1c
tommyxiao <xiaoke83@gmail.com>
2020-07-03 15:53:01 +0800

    use gradle to build project

---
+++ src/kexiao8/javatest/SpringDeferredResult.java
@@ -0,0 +1,71 @@
+package kexiao8.javatest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by tommyxiao on 5/6/20 21:04
+ * 演示在其他线程处理controller请求
+ */
+
+interface TaskService {
+    String execute();
+}
+
+@Service
+class TaskServiceImpl implements TaskService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String execute() {
+        try {
+            Thread.sleep(3000);
+            logger.info("Slow task executed");
+            return "Task finished";
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+}
+
+@SpringBootApplication
+@RestController
+public class SpringDeferredResult {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final TaskService taskService;
+
+    @Autowired
+    public SpringDeferredResult(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeferredResult.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.info("-----in hello process -----");
+        return String.format("Hello %s!", name);
+    }
+
+    @RequestMapping(value = "/deferred", method = RequestMethod.GET, produces = "text/html")
+    public DeferredResult<String> executeSlowTask() {
+        logger.info("Request received");
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        CompletableFuture.supplyAsync(taskService::execute)
+                .whenCompleteAsync((result, throwable) -> deferredResult.setResult(result));
+        logger.info("Servlet thread released");
+
+        return deferredResult;
+    }
+
+}
